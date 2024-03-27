973e0cc254ee8344f6dbd59d47867f17f543564e
Pooya Parsa <pooya@pi0.io>
2024-03-27 14:15:27 +0100

    test: add initial tests

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,8 +1,44 @@
 import { expect, it, describe } from "vitest";
-import {} from "../src";
+import { detectType } from "../src/detect";
+import { convertTo } from "../src/convert";
+import type { DataType, DataTypeName } from "../src/types";
 
-describe("undio", () => {
-  it.todo("pass", () => {
-    expect(true).toBe(true);
-  });
+const fixtures: Record<DataTypeName, DataType[]> = {
+  ArrayBuffer: [new ArrayBuffer(1)],
+  Blob: [new Blob()],
+  DataView: [new DataView(new ArrayBuffer(1))],
+  NumberArray: [[]],
+  ReadableStream: [new ReadableStream()],
+  Response: [new Response()],
+  String: ["string"],
+  Uint8Array: [new Uint8Array()],
+};
+
+const typeNames = Object.keys(fixtures) as DataTypeName[];
+
+describe("detectType", () => {
+  for (const [typeName] of Object.entries(fixtures)) {
+    describe(typeName, () => {
+      for (const input of fixtures[typeName as DataTypeName]) {
+        it(`should detect ${typeName} from ${input}`, () => {
+          expect(detectType(input)).toBe(typeName);
+        });
+      }
+    });
+  }
+});
+
+describe("convertTo", () => {
+  for (const from of typeNames) {
+    for (const to of typeNames) {
+      describe.skipIf(from === "ReadableStream")(`${from} to ${to}`, () => {
+        for (const input of fixtures[from]) {
+          it(`should convert ${from} to ${to}`, async () => {
+            const output = await convertTo(to, input);
+            expect(detectType(output)).toBe(to);
+          });
+        }
+      });
+    }
+  }
 });
