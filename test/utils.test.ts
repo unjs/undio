82a3ec835037854bc4c63703f54eb793be55b59d
ysknsid25 <kengo071225@gmail.com>
2025-04-07 21:33:01 +0900

    test: Improve tests coverage
    Signed-off-by: ysknsid25 <kengo071225@gmail.com>

--- test/utils.test.ts
+++ test/utils.test.ts
@@ -14,7 +14,7 @@ describe("_base64Encode", () => {
     expect(result).toBe("data:;base64,SGVsbG8");
   });
 
-  it("should encode Uint8Array to base64 URL safe string", () => {
+  it("should encode Uint8Array to base64 not dataURL", () => {
     const data = new Uint8Array([72, 101, 108, 108, 111]);
     const result = _base64Encode(data, { dataURL: false });
     expect(result).toBe("SGVsbG8=");
