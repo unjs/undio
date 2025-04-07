7f8f9ad9b919e615872939b87b0e67f407561e6c
ysknsid25 <kengo071225@gmail.com>
2025-04-07 21:19:39 +0900

    test: Improve tests coverage
    Signed-off-by: ysknsid25 <kengo071225@gmail.com>

---
+++ test/utils.test.ts
@@ -0,0 +1,28 @@
+import { describe, expect, it } from "vitest";
+import { _base64Encode } from "../src/data-types/_utils";
+
+describe("_base64Encode", () => {
+  it("should encode Uint8Array to base64 string", () => {
+    const data = new Uint8Array([72, 101, 108, 108, 111]);
+    const result = _base64Encode(data);
+    expect(result).toBe("data:;base64,SGVsbG8=");
+  });
+
+  it("should encode Uint8Array to base64 URL safe string", () => {
+    const data = new Uint8Array([72, 101, 108, 108, 111]);
+    const result = _base64Encode(data, { urlSafe: true });
+    expect(result).toBe("data:;base64,SGVsbG8");
+  });
+
+  it("should encode Uint8Array to base64 URL safe string", () => {
+    const data = new Uint8Array([72, 101, 108, 108, 111]);
+    const result = _base64Encode(data, { dataURL: false });
+    expect(result).toBe("SGVsbG8=");
+  });
+
+  it("should encode Uint8Array to base64 with custom type", () => {
+    const data = new Uint8Array([72, 101, 108, 108, 111]);
+    const result = _base64Encode(data, { type: "text/plain" });
+    expect(result).toBe("data:text/plain;base64,SGVsbG8=");
+  });
+});
